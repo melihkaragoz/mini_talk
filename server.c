2b9eef5118c3be383f2a8f4e1d8658189051924d
Melih Karagoz <mkaragoz@k1m42s05.42istanbul.com.tr>
2023-01-10 17:33:12 +0300

    ready to evo

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:56:04 by mkaragoz          #+#    #+#             */
-/*   Updated: 2023/01/10 16:02:13 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2023/01/10 17:32:09 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	ft_putchar(char c)
 
 void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i])
-		write(1,&str[i++],1);
+		write(1, &str[i++], 1);
 }
 
 void	ft_putnbr(int nb)
@@ -44,16 +44,16 @@ void	ft_putnbr(int nb)
 
 void	handle(int signum)
 {
-	static char	c = 0;
-	static int 	g_i = 128;
+	static char	c;
+	static int	i = 128;
 
 	if (signum == SIGUSR2)
-		c += g_i;
-	g_i /= 2;
-	if (g_i == 0)
+		c += i;
+	i /= 2;
+	if (i == 0)
 	{
-		write(1,&c,1);
-		g_i = 128;
+		write(1, &c, 1);
+		i = 128;
 		c = 0;
 	}
 }
